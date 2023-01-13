d507612eedb5a9ad1661a808fcd3e2a67d80948b
cvz5214 <72824123+cvz5214@users.noreply.github.com>
2023-01-12 21:29:23 -0500

    Adding videos for the website + placing them correctly in the html

--- script.js
+++ script.js
@@ -4,19 +4,177 @@
 */
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
-
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-var player;
+var vidFB1;
+var vidFB2;
+var vidFB3;
+
+var vidUB1;
+var vidUB2;
+var vidUB3;
+
+var vidLB1;
+var vidLB2;
+var vidLB3;
+
+var vidWC1;
+var vidWC2;
+var vidWC3;
+
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
+    // var = new YT.Payer('id in html', {video parameters})
+    vidFB1 = new YT.Player('vidFB1', {
         height: '390',
         width: '640',
-        videoId: 'M7lc1UVf-VE',
+        videoId: 'GViX8riaHX4',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    vidFB2 = new YT.Player('vidFB2', {
+        height: '390',
+        width: '640',
+        videoId: 'sinkIlViPG8',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    vidFB3 = new YT.Player('vidFB3', {
+        height: '390',
+        width: '640',
+        videoId: 'rI_6l992GrA',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    vidUB1 = new YT.Player('vidUB1', {
+        height: '390',
+        width: '640',
+        videoId: 'UEWEYeJGkLM',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    vidUB2 = new YT.Player('vidUB2', {
+        height: '390',
+        width: '640',
+        videoId: 'gy4vx4wQGDE',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    vidUB3 = new YT.Player('vidUB3', {
+        height: '390',
+        width: '640',
+        videoId: 'zZ8tWnE8kzQ',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+    
+    vidLB1 = new YT.Player('vidLB1', {
+        height: '390',
+        width: '640',
+        videoId: '0zhvUV1bAVQ',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    vidLB2 = new YT.Player('vidLB2', {
+        height: '390',
+        width: '640',
+        videoId: '-2DkbkIyq5c',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    vidLB3 = new YT.Player('vidLB3', {
+        height: '390',
+        width: '640',
+        videoId: 'WGA_ctAMkMk',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    vidWC1 = new YT.Player('vidWC1', {
+        height: '390',
+        width: '640',
+        videoId: '2pLT-olgUJs',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    vidWC2 = new YT.Player('vidWC2', {
+        height: '390',
+        width: '640',
+        videoId: '2MoGxae-zyo',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    vidWC3 = new YT.Player('vidWC3', {
+        height: '390',
+        width: '640',
+        videoId: '31BiQiXk8jQ',
         playerVars: {
             'playsinline': 1
         },
@@ -29,7 +187,10 @@ function onYouTubeIframeAPIReady() {
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-    event.target.playVideo();
+    // event.target.playVideo();
+    if (event.data == YT.PlayerState.PLAYING) {
+        stopVideo();
+    }
 }
 
 // 5. The API calls this function when the player's state changes.
@@ -38,14 +199,14 @@ function onPlayerReady(event) {
 var done = false;
 function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
+        // setTimeout(stopVideo, 6000);
         done = true;
     }
 }
 function stopVideo() {
     player.stopVideo();
 }
-// Code for youtube player finished
+// End of code for youtube player
 
 /* __   __   __   __        ___                __   __  
   / _` /  \ /  \ / _` |    |__      |\/|  /\  |__) /__` 
@@ -96,3 +257,4 @@ google.maps.event.addDomListener(window, "load", function () {
         map: map
     }, "<h1>Marker 2</h1><p>This is marker 2</p>");
 });
+// End of code for google maps
